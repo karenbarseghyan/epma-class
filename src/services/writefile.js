d6624b9cb193711ee73b80e5c718f577259ecc55
karen_barseghyan <karenbarseghyan90@gmail.com>
2021-08-22 00:22:06 +0400

    Deleting from elder files

--- src/services/writefile.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var fs = require("fs");
-var output = fs.createWriteStream('../data/main.txt');
-output.on('finish', function () {
-    console.log('input finish');
-});
--- src/services/writefile.ts
+++
@@ -1,7 +0,0 @@
-import * as fs from 'fs'
-
-const output = fs.createWriteStream('../data/main.txt')
-
-output.on('finish', ()=> {
-    console.log('input finish')
-} )
