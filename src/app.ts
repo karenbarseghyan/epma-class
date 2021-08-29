bbb89dee3006889675ed394fe91290d876c675a4
karen_barseghyan <karenbarseghyan90@gmail.com>
2021-08-29 18:08:34 +0400

    TypeORM set-up

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,16 @@
 import express, {Express, Response, Request} from 'express';
+import { createConnection } from 'typeorm';
+import {User} from './entities/user'
+
+createConnection({
+  type: 'postgres',
+  database: "rainbow_database",
+  username: "unicorn_user",
+  password: "magical_password",
+  logging: true,
+  synchronize: true,
+  entities: [User]
+})
 
 const app = express()
 const port = 3000
